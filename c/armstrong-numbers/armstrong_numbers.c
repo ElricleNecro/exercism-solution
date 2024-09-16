2677b51cfc84f071f5d52a772682c9b26b3d0635
Guillaume Plum <guiplum@gmail.com>
2024-09-16 20:25:33 +0200

    feat(c,armstrong): adding my solution

--- c/armstrong-numbers/armstrong_numbers.c
+++ c/armstrong-numbers/armstrong_numbers.c
@@ -1,5 +1,31 @@
 #include "armstrong_numbers.h"
 
-bool is_armstrong_number(int candidate) {
+#include <stdio.h>
 
+static int powi(int a, int b) {
+	int res = a;
+	while( b > 1 ) {
+		res *= a;
+		b--;
+	}
+
+	return res;
+}
+
+bool is_armstrong_number(const int candidate) {
+	int res = 0, nb = 1;
+
+	int tmp = candidate;
+	while( tmp > 10 ) {
+		tmp /= 10;
+		nb++;
+	}
+
+	tmp = candidate;
+	while( tmp > 0 ) {
+		res += powi(tmp % 10, nb);
+		tmp /= 10;
+	}
+
+	return res == candidate ? true : false;
 }
