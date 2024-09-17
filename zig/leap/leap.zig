6b9c3d1c98ff442a2a4dcec381c822bca117fd89
Guillaume Plum <guillaume.plum@iktos.com>
2024-09-17 11:14:41 +0200

    feat(zig,leap): adding zig leap solution

---
+++ zig/leap/leap.zig
@@ -0,0 +1,9 @@
+pub fn isLeapYear(year: u32) bool {
+    if( year % 100 == 0 and year % 400 == 0 ) {
+        return true;
+    } else if( year % 4 == 0 and year % 100 != 0 ) {
+        return true;
+    }
+
+    return false;
+}
