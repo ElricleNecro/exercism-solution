e1fb596aaeaca6c300e2f29f18452699326ec21c
Guillaume Plum <guillaume.plum@iktos.com>
2024-09-18 13:30:50 +0200

    refacto(haskell,leap): 3rd iteration
    Learning more haskell

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -1,5 +1,9 @@
 module LeapYear (isLeapYear) where
 
 isLeapYear :: Integer -> Bool
-isLeapYear year = divisibleBy 4 && (not(divisibleBy 100) || divisibleBy 400)
-	where divisibleBy d = year `mod` d == 0
+isLeapYear year
+  | divisibleBy 4 && not(divisibleBy 100) = True
+  | divisibleBy 400 = True
+  | otherwise = False
+  where
+    divisibleBy d = year `mod` d == 0
