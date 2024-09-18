0602420cb759b3fe6687edc209c409a54cc666d6
Guillaume Plum <guiplum@gmail.com>
2024-09-18 07:45:52 +0200

    feat(haskell,reverse): (adding new variant for curiosity)

--- haskell/reverse-string/src/ReverseString.hs
+++ haskell/reverse-string/src/ReverseString.hs
@@ -2,3 +2,6 @@ module ReverseString (reverseString) where
 
 reverseString :: String -> String
 reverseString = reverse
+
+revStr :: String -> String
+revStr str = if length str == 0 || length str == 1 then str else (last str) : (revStr (init str))
